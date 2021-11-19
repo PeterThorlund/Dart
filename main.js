05030ec9fff0ad79d1dcd348486588a5a3827f8c
(your name) <(your mail address)>
2021-11-19 09:39:12 +0100

    player 1 virker næsten

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
 class Player {
-    constructor() {
+    constructor(name) {
         this.score = 501
         this.noOfSetWon = 0;
+        this.name = name
     }
 
     resetCount() {
@@ -16,14 +17,29 @@ class Player {
         return this.noOfSetWon;
     }
 
+    setScore(i) {
+        this.score -= i;
+    }
+
 }
 
 let app = new Vue({
     el: '#app',
     data: {
-        player: new Player(),
+        player1: new Player("player1"),
+        player2: new Player("player2"),
         input: ""
     },
     methods: {
+        onSubmit: function() {
+            if(check(this.player1.score,this.input)) {
+                this.player1.setScore(+this.input);
+                this.input = "";
+            }
+        }
     }
 })
+
+function check(score, hit) {
+    return (score - hit >= 0) && (score - hit != 1) && (hit <= 180) && (hit <= score)
+  }
